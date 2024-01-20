0c5062c4ddd57546b524e219973092fa4f1f173b
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2024-01-20 13:56:36 -0500

    Container added.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container" style="height:842px;width:595px;padding-left:8px;margin:auto;">
+    <iframe src="" seamless style="border:1px solid #000;height:842px;width:595px;padding-left:8px;"></iframe>
+  </div>
+  <?php
+    
+  ?>
+</body>
+</html>
