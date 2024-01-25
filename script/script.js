c39496d26d043c9f0ed6c46527bdeabadf65861e
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2024-01-25 11:25:25 -0500

    Option to print added.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-// signature
+// Signature
 (function () {
   const signCanvas = document.getElementById('sign-here');
   var btn = document.getElementById('sign-button');
@@ -30,8 +30,8 @@
   }
 
   function onSigStart(ev) {
-    console.log('Signature starting');
-    console.log(ev.type);
+    // console.log('Signature starting');
+    // console.log(ev.type);
     var coords = getOffset(ev);
     if (!coords) {
         return;
@@ -42,8 +42,8 @@
   }
 
   function onSigMove(ev) {
-    console.log('Signature moving');
-    console.log(ev.type);
+    // console.log('Signature moving');
+    // console.log(ev.type);
     if (!drawing) {
         return;
     }
@@ -112,3 +112,108 @@ const IMappedEvents = {
   move: [],
   end: []
 };
+
+// Load teeth image on canvas
+const teethImg = new Image();
+teethImg.src = "images/teeth-10.svg";
+teethImg.onload = () => {
+  ctx.drawImage(teethImg, 0, 0, 315, 657);    
+}
+
+// Drawing on teeth
+const teethCanvas = document.getElementById("teethCanvas");
+const toolbar = document.getElementById('toolbar');
+const ctx = teethCanvas.getContext('2d');
+
+const canvasOffsetX = teethCanvas.offsetLeft;
+const canvasOffsetY = teethCanvas.offsetTop;
+
+teethCanvas.width = 315;
+teethCanvas.height = 657;
+
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
+
+toolbar.addEventListener('click', ev => {
+  ev.preventDefault();
+  const teethImg = new Image();
+  teethImg.src = "images/teeth-10.svg";
+  
+    if (ev.target.id === 'clear') {   
+    teethImg.onload = () => {
+      ctx.drawImage(teethImg, 0, 0, 315, 657);    
+    }
+    ctx.clearRect(teethImg, 0, 0, teethCanvas.width, teethCanvas.height);
+    }
+});
+
+toolbar.addEventListener('change', ev => {
+    if(ev.target.id === 'stroke') {
+        ctx.strokeStyle = ev.target.value;
+    }
+
+    if(ev.target.id === 'lineWidth') {
+        lineWidth = ev.target.value;
+    }
+    
+});
+
+const draw = (ev) => {
+    if(!isPainting) {
+        return;
+    }
+
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+
+    ctx.lineTo(ev.clientX - canvasOffsetX, ev.clientY);
+    ctx.stroke();
+}
+
+teethCanvas.addEventListener('mousedown', (ev) => {
+    isPainting = true;
+    startX = ev.clientX;
+    startY = ev.clientY;
+    ctx.lineCap = 'round';
+    ctx.beginPath();
+    ctx.moveTo(startX - canvasOffsetX, startY);
+});
+
+teethCanvas.addEventListener('mouseup', ev => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+
+teethCanvas.addEventListener('mousemove', draw);
+
+// TODO: Save as PDF
+function exportHTMLtoPDF() {
+  let htmlElement = document.getElementById('content');
+  //office 
+  const office = document.getElementById('office');
+
+  //patient-name ??? TODO: think need full name: separate full name as first and last name
+  const patient = document.getElementById('patient');
+
+  //date ?? TODO: ask if they want to have date or date-requested, and if they want to have time, like hours:minutes
+  const date = document.getElementById('date');
+
+  //TODO: Get the actual time somehow to put in the name of the file
+  // const time = 
+
+  html2pdf().from(htmlElement).save('office_patient-name_date_time.pdf');
+ 
+  // html2pdf().from(htmlElement).save(office + '_' + patient + '_' + date + '_' + time + '.pdf');
+}
+
+document.getElementById('saveButton').addEventListener('click', exportHTMLtoPDF);
+
+//TODO: Save the form on the server ( subject to discuss). Find out how
+
+//TODO: Send to the pdf file to default email address (provided by us) and to the client (email proved in the form) - Email in the form, ask somewhere the person who fills out the form about their email
+//Idea: On submit ask the person of their email
+
+
